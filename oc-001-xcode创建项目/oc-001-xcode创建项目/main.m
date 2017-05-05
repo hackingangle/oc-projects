09aea47e2777f40778fa94397483582d6c60f5c3
hackingangle <hackingangle@gmail.com>
2017-05-05 08:22:24 +0800

    xcode使用，创建第一个项目

---
+++ oc-001-xcode创建项目/oc-001-xcode创建项目/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  oc-001-xcode创建项目
+//
+//  Created by wanggang on 2017/5/5.
+//  Copyright © 2017年 hackingangle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
